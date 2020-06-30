8223c1415869abbfeeefa72136125748fecb5ce8
Veronika Shumilova <mojjevelovii@gmail.com>
2020-06-30 13:58:48 +0300

    реализована навигация по задачам

--- app/src/main/java/ru/shumilova/popular_libs/MainActivity.java
+++ app/src/main/java/ru/shumilova/popular_libs/MainActivity.java
@@ -2,13 +2,52 @@ package ru.shumilova.popular_libs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button task1;
+    private Button task2;
+    private Button task3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initButtons();
+    }
+
+    private void initView() {
+        task1 = findViewById(R.id.b_task_1);
+        task2 = findViewById(R.id.b_task_2);
+        task3 = findViewById(R.id.b_task_3);
+    }
+
+    private void initButtons() {
+        task1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Task1Activity.class));
+            }
+        });
+
+        task2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Task2Activity.class));
+            }
+        });
+
+        task3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Task3Activity.class));
+            }
+        });
     }
 }
