a555179e71b9064ac58a1a9b86a6112a1e6981e8
Veronika Shumilova <mojjevelovii@gmail.com>
2020-06-30 13:18:34 +0300

    создание и разметка активити

---
+++ app/src/main/java/ru/shumilova/popular_libs/Task2Activity.java
@@ -0,0 +1,14 @@
+package ru.shumilova.popular_libs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_2);
+    }
+}
