952750b0e6d9dc8661c4493a6b355bdc34a04f44
Veronika Shumilova <mojjevelovii@gmail.com>
2020-07-27 20:25:16 +0300

    Задача 1.

--- app/src/main/java/ru/shumilova/popular_libs/MainActivity.java
+++ app/src/main/java/ru/shumilova/popular_libs/MainActivity.java
@@ -3,12 +3,42 @@ package ru.shumilova.popular_libs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterText;
+    private TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        enterText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showText.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        enterText = findViewById(R.id.et_enter_text);
+        showText = findViewById(R.id.tv_show_text);
     }
 }
